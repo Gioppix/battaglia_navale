85f87bd523c1f33fb54b734d841d06cc8f684390
TheGioppix25 <gfeltrin25@gmail.com>
2021-12-30 21:08:23 +0100

    updated test

--- recursion_test.js
+++ recursion_test.js
@@ -25,12 +25,12 @@ m0 = [
       0, 0, 0, 3, 0
     ],
     [
-      0, 0, 0, 0, 0,
+      3, 0, 0, 0, 0,
       0, 3, 0, 0, 0
     ],
     [
       0, 0, 0, 0, 0,
-      0, 3, 3, 3, 3
+      3, 3, 3, 3, 3
     ],
     [
       0, 0, 0, 0, 0,
@@ -57,38 +57,39 @@ resoo
 puro0
 
 function checkAd(i, j, m, reso, puro){
-    //console.log(m[i]);
-    console.log(m[i][j])
-    if(m[i][j] == 0 || m[i][j] == 1 ||  m[i][j] == 4 ){
-        return;
-    }
-    if(m[i][j] == 2){
-        puro.p = false;
-        return;
-    }
-    if(m[i][j] == 3){
-        reso.push("" + i + j);
-        //return;
-    }
-    console.log(!reso.includes("" + i + j))
-    if(checkCell(i + 1, j) && !reso.includes("" + (i + 1) + j)){
-        console.log("" + (i + 1) + j)
-        checkAd(i + 1, j, m, reso, puro);
-    }
-    if(checkCell(i, j - 1) && !reso.includes("" + i + (j - 1))){
-        console.log("" + i + (j - 1))
-        checkAd(i, j - 1, m, reso, puro);
-    }
-    if(checkCell(i - 1, j) && !reso.includes("" + (i - 1) + j)){
-        console.log("" + (i - 1) + j)
-        checkAd(i - 1, j, m, reso, puro);
-    }
-    if(checkCell(i, j + 1) && !reso.includes("" + i + (j + 1))){
-        console.log("" + i + (j + 1))
-        checkAd(i, j + 1, m, reso, puro);
-    }
+  //console.log(m[i]);
+  //console.log("i, j: "+ i + j + " v: " + m[i][j])
+  if(m[i][j] == 0 || m[i][j] == 1 ||  m[i][j] == 4 ){
+      //console.log("finito")
+      return;
+  }
+  if(m[i][j] == 2){
+      puro.p = false;
+      return;
+  }
+  if(m[i][j] == 3){
+      reso.push("" + i + j);
+      //return;
+  }
+  if(checkCell(i - 1, j) && !reso.includes("" + (i - 1) + j)){
+      checkAd(i - 1, j, m, reso, puro);
+      //console.log("3 if")
+  }
+  // - 0 per forzarli a int
+  //console.log("check ", "" + i + (j + 1))
+  if(checkCell(i, j - 0 + 1) && !reso.includes("" + i + (j - 0 + 1))){
+      checkAd(i, j - 0 + 1, m, reso, puro);
+      //console.log("4 if")
+  }
+  if(checkCell(i - 0 + 1, j) && !reso.includes("" + (i - 0 + 1) + j)){
+      checkAd(i - 0 + 1, j, m, reso, puro);
+      //console.log("prima if")
+  }
+  if(checkCell(i, j - 1) && !reso.includes("" + i + (j - 1))){
+      checkAd(i, j - 1, m, reso, puro);
+      //console.log("2 if")
+  }
 }
-
 function checkCell(x, y){
     console.log(x, y)
     if(x >= 0 && x < 10 && y >= 0 && y < 10){
